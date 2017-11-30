aea5ef42992356cce54f4b4d8f7b331e878d86f7
Draco95 <daniele.dallolio95@gmail.com>
2017-11-30 01:30:31 +0100

    Added scripts folder

---
+++ scripts/downloadDecompressMergeFastq.py
@@ -0,0 +1,135 @@
+def downloadDecompressMergeFastq(name, all_fastq, fastq_path, fastq_id, homepath, processpath,logpath, cadaver=False):
+
+    import os
+    import re
+    import subprocess as sp
+
+    inpath = fastq_path
+    inoutpath = processpath + '01_fastq/01_preprocess/'
+    outpath = processpath + '01_fastq/02_postprocess/'
+    logpath = logpath
+
+    # create fastq names
+    fastq1 = name + '_R1_unchecked.fastq'
+    fastq2 = name + '_R2_unchecked.fastq'
+
+
+    # in case 'cadaver' option was chosen
+    if cadaver == True:
+
+        inpath = inpath.replace('NAS/', '')
+
+        # import list of all fastq
+        with open(homepath + all_fastq) as infile:
+            all_fastq = infile.read().splitlines()
+
+        # list all fastq.gz files that contain fastq_id
+        gz_list = [gz for gz in all_fastq if re.search(fastq_id, gz)]
+
+        # open file connection
+        with open(processpath + 'cadaver_get.sh', 'w') as outfile:
+
+            # loop over fastq files of current sample
+            for gz in gz_list:
+
+                # add a 'get' command for each fastq file, each in a different line
+                outfile.write(' '.join(['get',
+                                        inpath + gz,
+                                        inoutpath + gz + '\n']))
+
+            # add last line to close connection
+            outfile.write('quit')
+
+        # run cadaver script
+        sp.call(' '.join(['cadaver',
+                          'https://ngs-ptl.unibo.it:5006',
+                          '-r', processpath + 'cadaver_get.sh',
+                          '>', logpath + name + '_cadaver.log']), shell=True)
+
+        # delete script
+        sp.call(' '.join(['rm', processpath + 'cadaver_get.sh']), shell=True)
+
+        # loop over fastq
+        for gz in gz_list:
+
+            # decompress it
+            sp.call(' '.join(['gzip',
+                              '-d', inoutpath + gz]), shell=True)
+
+    else:
+
+        # complete fastq_path
+        fastq_path = homepath + fastq_path
+
+        # list all fastq.gz files that contain fastq_id
+        gz_list = [gz for gz in os.listdir(inpath) if re.search(fastq_id, gz)]
+
+        # loop over them
+        for gz in gz_list:
+
+            # decompress it while downloading to inoutpath
+            sp.call(' '.join(['gzip',
+                              '-dc', inpath + gz,
+                              '>', inoutpath + gz[:-3]]), shell=True)
+
+
+
+
+
+
+    # list common substring of each pair of fastq
+    common_substrings = list(set([re.split('_R[12]', f)[0] for f in os.listdir(inoutpath)]))
+
+    # list all R1 fastq (unordered), concatenating their relative path
+    R1 = [inoutpath + f for f in os.listdir(inoutpath) if re.search('R1', f)]
+
+    # list all R2 fastq (unordered), concatenating their relative path
+    R2 = [inoutpath + f for f in os.listdir(inoutpath) if re.search('R2', f)]
+
+    # create new lists
+    R1b = []
+    R2b = []
+
+    # loop over common substrings (pairs of fastqs)
+    for cs in common_substrings:
+
+        # loop over R1 fastq
+        for r1 in R1:
+
+            # if common substring matches R1 fastq
+            if re.search(cs, r1):
+
+                # append it to new list
+                R1b.append(r1)
+
+        # loop over R1 fastq
+        for r2 in R2:
+
+            # if common substring matches R1 fastq
+            if re.search(cs, r2):
+
+                # append it to new list
+                R2b.append(r2)
+
+    # join elements in new lists
+    R1b = ' '.join(R1b)
+    R2b = ' '.join(R2b)
+
+    # concatenate multiple fastq (if there are)
+    sp.call(' '.join(['cat', R1b, '>', outpath + fastq1]), shell=True)
+    sp.call(' '.join(['cat', R2b, '>', outpath + fastq2]), shell=True)
+
+    # delete fastq in INOUTPATH
+    sp.call(' '.join(['rm', inoutpath + '*']), shell=True)
+
+
+# Snakemake call
+
+downloadDecompressMergeFastq(snakemake.params['name'],
+                             snakemake.params['all_fastq'],
+                             snakemake.params['fastq_path'],
+                             snakemake.params['fastq_id'],
+                             snakemake.params['homepath'],
+                             snakemake.params['processpath'],
+                             snakemake.log,
+                             snakemake.params['cadaver'])
---
+++ scripts/editBEDChromField.py
@@ -0,0 +1,30 @@
+# The WES reference BED files from Illumina have the chromosome names as 'chr#', 'chrX', 'chrY', 'chrM' and before running the pipeline we must change them to '1', '2', .. , 'Y', 'MT'
+
+import pandas as pd
+
+def editBEDChromField(input_bed, line_to_skip,output_bed):
+    # get name of BED file from command line
+    bed_name = input_bed
+
+    # some files have a first line that we have to skip
+    # if no 2nd argument is provided, skip no lines
+    # otherwise skip the line number provided in the 2nd argument
+    rows_to_skip = None
+    if line_to_skip:
+        rows_to_skip = [int(r) for r in line_to_skip]
+
+    # load file
+    bed = pd.read_table(bed_name, sep='\t', header=None, dtype='str', skiprows=rows_to_skip)
+
+    # change 'chr#' to '#' and 'chrM' to 'MT'
+    bed[0] = ['MT' if c == 'chrM' else c[3:] for c in bed[0]]
+
+    # cut extension
+    bed_name = bed_name[:-4]
+
+    # write to file
+    bed.to_csv(output_bed, sep='\t', header=False, index=False)
+
+editBEDChromField(snakemake.input,
+                  snakemake.params['line_to_skip'],
+                  snakemake.output)

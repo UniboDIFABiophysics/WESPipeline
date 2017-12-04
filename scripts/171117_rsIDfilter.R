1351e57f178e6d19aa5b632c6da47be779a4ffbf
Draco95 <daniele.dallolio95@gmail.com>
2017-12-04 16:04:47 +0100

    Changed arguments

--- scripts/171117_rsIDfilter.R
+++ scripts/171117_rsIDfilter.R
@@ -5,34 +5,39 @@
 # 'arg1' is required and must be the whole path of input rsID list file
 # input file must be called 'rsID.tsv'
 
-library(rsnps)
+rsIDfilter <- function(input_table,outtable) {
 
-# store command line arguments
-args = commandArgs(trailingOnly = TRUE)
+  library(rsnps)
 
-# load rsID list from file
-rsIDs = read.delim(paste0(args[1], 'rsID.tsv'), sep="\t", header=F, quote="", stringsAsFactors=F)
+  # store command line arguments
+  args = commandArgs(trailingOnly = TRUE)
 
-# the number of rsIDs that can be passed in to one request is limited to around 600
-# therefore, if there are more than 500 I split them in group of 500 and make multiple queries
-if(length(rsIDs)>500){
-  
-  rsIDs_annotated=NULL
-  
-  n = length(rsIDs)
-  groups = (n%/%500)+1
-  
-  for(i in 1:groups){
-    l1 = (500*(i-1))+1
-    if(i<groups){l2 = 500*i}else{l2 = n}
-    ids = rsIDs[l1:l2]
-    rsIDs_annotated<-rbind(rsIDs_annotated, ncbi_snp_query(ids))
-  }
-  
-}else{rsIDs_annotated<-ncbi_snp_query(rsIDs)}
+  # load rsID list from file
+  rsIDs = read.delim(paste0(args[1], input_table), sep="\t", header=F, quote="", stringsAsFactors=F)
 
-# write MAF annotation to file
-write.table(rsIDs_annotated, file=paste0(args[1], 'rsID_maf.tsv'), sep="\t", row.names=F, col.names=T, dec=".", quote=F)
+  # the number of rsIDs that can be passed in to one request is limited to around 600
+  # therefore, if there are more than 500 I split them in group of 500 and make multiple queries
+  if(length(rsIDs)>500){
+
+    rsIDs_annotated=NULL
+
+    n = length(rsIDs)
+    groups = (n%/%500)+1
+
+    for(i in 1:groups){
+      l1 = (500*(i-1))+1
+      if(i<groups){l2 = 500*i}else{l2 = n}
+      ids = rsIDs[l1:l2]
+      rsIDs_annotated<-rbind(rsIDs_annotated, ncbi_snp_query(ids))
+    }
+
+  }else{rsIDs_annotated<-ncbi_snp_query(rsIDs)}
+
+  # write MAF annotation to file
+  write.table(rsIDs_annotated, file=paste0(args[1], outtable), sep="\t", row.names=F, col.names=T, dec=".", quote=F)
 
 
-sessionInfo()
+  sessionInfo()
+}
+
+rsIDfilter(snakemake@input[['not_filtered']], snakemake@output[['out']])

1ef108921647ff48f7eefdd0c78f3642dab8aee9
Draco95 <daniele.dallolio95@gmail.com>
2017-12-09 09:24:17 +0100

    Removed useless commands

--- install.sh
+++ install.sh
@@ -10,16 +10,10 @@ pip install -U snakemake
 pip install -U PyYAML
 pip install matplotlib
 pip install pandas
-pip pysam
 pip install ipython
 conda config --add channels bioconda
+conda install graphviz -y
 mkdir ~/data_ref
-scp /mnt/avoton/biofisici/data_snakemake/human_g1k_v37.fasta.gz ~/data_ref/
-scp /mnt/avoton/biofisici/data_snakemake/SRR1611178_1.fastq.gz ~/data_ref/
-scp /mnt/avoton/biofisici/data_snakemake/SRR1611178_2.fastq.gz ~/data_ref/
-gunzip ~/data_ref/*.gz
-rm ~/biopipe/Miniconda3-latest-Linux-x86_64.sh
-chmod ugo+x script.sh
 echo " " >> ~/.bashrc
 echo "PATH=~/miniconda3/bin:$PATH"  >> ~/.bashrc
 
